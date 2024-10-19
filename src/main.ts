d24dcabfd6d01fa67863c9d72fe8ab7b8994c8bc
jfsicilia <jfsicilia@gmail.com>
2024-10-19 08:49:05 +0100

    Added settings to enable/disable plugin. Better support for vimium.

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,43 @@
-import { Plugin, Setting, View, MarkdownView } from "obsidian";
+import {
+	Notice,
+	Plugin,
+	App,
+	PluginSettingTab,
+	Setting,
+	View,
+	WorkspaceItem,
+} from "obsidian";
 
 // Remember to rename these classes and interfaces!
 
 interface KeyboardEnhancerSettings {
-	mySetting: string;
+	enableEnhancer: boolean;
 }
 
 const DEFAULT_SETTINGS: KeyboardEnhancerSettings = {
-	mySetting: "default",
+	enableEnhancer: true,
 };
+
+// "file-properties", "outline", "localgraph", "all-properties", "tag", "backlink",
+// "recent-files", "bookmarks", "search"
+// const VIMIUM_COMPATIBLE_VIEW: string[] = [
+// 	"external-links-view",
+// 	"file-explorer",
+// 	"file-properties",
+// 	"localgraph",
+// 	"tag",
+// 	"all-properties",
+// 	"outline",
+// 	"outgoing-link",
+// 	"recent-files",
+// 	"bookmarks",
+// 	"backlink",
+// ];
+
+// const VIMIUM_NOT_AVAILABLE_CLASSNAMES: string[] = [
+// 	"cm-content",
+// 	"inline-title",
+// ];
 
 export default class KeyboardEnhancerPlugin extends Plugin {
 	settings: KeyboardEnhancerSettings;
@@ -26,8 +55,10 @@ export default class KeyboardEnhancerPlugin extends Plugin {
 		);
 	}
 
-	inSplit(e, split): boolean {
+	inSplit(e: WorkspaceItem, split: WorkspaceItem): boolean {
 		while (e) {
+			console.log(e);
+			console.log(split);
 			if (e === split) return true;
 			e = e.parent;
 		}
@@ -44,24 +75,30 @@ export default class KeyboardEnhancerPlugin extends Plugin {
 			this.app.workspace.leftSplit.expand();
 		this.app.workspace.setActiveLeaf(view, { focus: true });
 	}
-	// "file-properties", "outline", "localgraph", "all-properties", "tag", "backlink",
-	// "recent-files", "bookmarks", "search"
 
-	vimiumCompatibleView: string[] = [
-		"external-links-view",
-		"file-properties",
-		"localgraph",
-		"tag",
-		"all-properties",
-		"outgoing-link",
-		"recent-files",
-		"bookmarks",
-		"outline",
-		"backlink",
-	];
+	async showVimiumMarkers() {
+		await this.app.commands.executeCommandById("vimium:show-markers");
+	}
+
+	hideVimiumMarkers() {
+		const escEvent = new KeyboardEvent("keydown", {
+			key: "Escape",
+			code: "Escape",
+			keyCode: 27, // keyCode for 'Escape'
+			bubbles: true,
+			cancelable: true,
+		});
+
+		// Dispatch the 'Escape' event on the document
+		document.dispatchEvent(escEvent);
+	}
 
 	async onload() {
+		const app = this.app;
+		const workspace = app.workspace;
+
 		await this.loadSettings();
+		this.addSettingTab(new KeyboardEnhancerSettingTab(this.app, this));
 
 		this.addCommand({
 			id: "focus-note",
@@ -89,27 +126,65 @@ export default class KeyboardEnhancerPlugin extends Plugin {
 				this.focusViewByType("external-links-view");
 			},
 		});
+		this.addCommand({
+			id: "toggle-keyboard-enhancer",
+			name: "Toggles on/off keyboard enhancer",
+			callback: async () => {
+				this.settings.enableEnhancer = !this.settings.enableEnhancer;
+				this.saveData(this.settings);
+				new Notice(
+					`Keyboard Enhancer is now ${
+						this.settings.enableEnhancer ? "enabled" : "disabled"
+					}`
+				);
+			},
+		});
 
 		this.registerDomEvent(window, "keydown", async (event) => {
+			// Do nothing if keyboard enhancer is disabled
+			if (!this.settings.enableEnhancer) return;
+
 			if (event.key === " ") {
 				if (this.isFileExplorerActive())
-					await this.app.commands.executeCommandById(
+					await app.commands.executeCommandById(
 						"file-manager:toggle-select"
 					);
 			}
-			if (event.key === "f") {
-				const viewType =
-					this.app.workspace
-						.getActiveViewOfType(View)
-						?.getViewType() ?? "";
-				if (
-					this.isMarkdownEditorActiveAndInPreviewMode() ||
-					this.vimiumCompatibleView.includes(viewType)
-				) {
-					await this.app.commands.executeCommandById(
-						"vimium:show-markers"
-					);
-				}
+			if (event.key === ";") {
+				const focusedElem = document.activeElement;
+				// Do nothing if focused element is an input or contenteditable
+				if (focusedElem?.tagName.toLowerCase() === "input") return;
+				if (focusedElem?.contentEditable === "true") return;
+				// Activate vimium if not showing markers, deactivate if showing
+				!document.querySelector(".vimium-container")
+					? await this.showVimiumMarkers()
+					: this.hideVimiumMarkers();
+
+				// if (
+				// 	VIMIUM_NOT_AVAILABLE_CLASSNAMES.some((classname) =>
+				// 		active?.classList.contains(classname)
+				// 	)
+				// )
+				// return;
+
+				// if (!document.querySelector(".vimium-container"))
+				// 	await this.showVimiumMarkers();
+				// else if (!document.querySelector(".vimium-marker-char-match"))
+				// 	this.hideVimiumMarkers();
+
+				// const viewType =
+				// 	workspace.getActiveViewOfType(View)?.getViewType() ?? "";
+				// if (
+				// 	this.isMarkdownEditorActiveAndInPreviewMode() ||
+				// 	VIMIUM_COMPATIBLE_VIEW.includes(viewType)
+				// ) {
+				// 	if (!document.querySelector(".vimium-container"))
+				// 		await this.showVimiumMarkers();
+				// 	else if (
+				// 		!document.querySelector(".vimium-marker-char-match")
+				// 	)
+				// 		this.hideVimiumMarkers();
+				// }
 			}
 		});
 	}
@@ -126,5 +201,32 @@ export default class KeyboardEnhancerPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
+	}
+}
+
+export class KeyboardEnhancerSettingTab extends PluginSettingTab {
+	plugin: KeyboardEnhancerPlugin;
+
+	constructor(app: App, plugin: KeyboardEnhancerPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		let { containerEl } = this;
+
+		containerEl.empty();
+
+		new Setting(containerEl)
+			.setName("Enable Keyboard Enhancer")
+			.setDesc("Allows to use keyboard shortcuts to navigate the app")
+			.addToggle((toggle) =>
+				toggle
+					.setValue(this.plugin.settings.enableEnhancer)
+					.onChange(async (value) => {
+						this.plugin.settings.enableEnhancer = value;
+						await this.plugin.saveSettings();
+					})
+			);
 	}
 }
